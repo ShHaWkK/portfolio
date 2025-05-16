d34efaf8697682bc0b3cd091102850c14a3b7893
Alexandre Uzan <alexandreuzan9@gmail.com>
2025-05-16 23:09:16 +0200

    update 3

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Tout autre réglage Next.js ici,
+  // mais plus besoin de experimental.appDir
+};
+
+module.exports = nextConfig;
